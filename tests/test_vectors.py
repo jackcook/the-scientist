5b2553b49ac712999aee7fe6572d7dca02ebf572
Jack Cook <hello@jackcook.nyc>
2016-08-16 17:14:00 -0400

    Updated a calculated value model

--- tests/test_vectors.py
+++ tests/test_vectors.py
@@ -1,6 +1,6 @@
 from scientist import answer_question
 
-def test_question_1():
+def test_question_1a():
     question = "A vector is given by its components, A(x) = 2.5 and A(y) = 7.5. What angle does vector A make with the positive x-axis?"
     answer = answer_question(question)
     assert answer == "72 degrees"
@@ -25,12 +25,17 @@ def test_question_1e():
     answer = answer_question(question)
     assert answer == "72 degrees"
 
-def test_question_3():
+def test_question_3a():
     question = "A vector has a magnitude of 17 units and makes an angle of 20 degrees with the positive x-axis. The magnitude of the horizontal component of this vector is"
     answer = answer_question(question)
     assert answer == "16 units"
 
-def test_question_4():
+def test_question_3b():
+    question = "A vector with a magnitude of 17 units makes an angle of 20 degrees with the positive x-axis. The magnitude of the horizontal component of this vector is"
+    answer = answer_question(question)
+    assert answer == "16 units"
+
+def test_question_4a():
     question = "As the angle between a given vector and the horizontal axis increases from 0 degrees to 90 degrees, the magnitude of the vertical component of this vector"
     answer = answer_question(question)
     assert answer == "increases"
