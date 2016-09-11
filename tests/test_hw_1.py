a45f378138542e758b0216f69fa77accd995d4e3
Jack Cook <jackcook@users.noreply.github.com>
2016-09-10 23:24:32 -0400

    Add rewording of test

--- tests/test_hw_1.py
+++ tests/test_hw_1.py
@@ -8,6 +8,10 @@ def test_question_2():
     question = "How many years are there in one 41-minute class period?"
     assert answer(question) == "7.8 × 10^-5 years"
 
+def test_question_2_1():
+    question = "How many years are there in 41 minutes?"
+    assert answer(question) == "7.8 × 10^-5 years"
+
 def test_question_4a():
     question = "There are 1.61 km in 1 mile. How many km/h is 50 mph?"
     assert answer(question) == "80.5 km/h"
